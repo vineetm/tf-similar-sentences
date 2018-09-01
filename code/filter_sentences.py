0d2e4ac001616d0504ecd500af9082fdbf1028ea
Vineet Kumar <vineeku6@in.ibm.com>
2018-09-01 11:31:20 +0530

    filter empty sentences, one word sentence, and some known markup patterns

---
+++ code/filter_sentences.py
@@ -0,0 +1,35 @@
+import argparse
+import tensorflow as tf
+
+logging = tf.logging
+logging.set_verbosity(logging.INFO)
+
+IGNORE_START = set('= * - ? } # : | colspan= align= ( alignbars style= − rowspan= ( _ ;'.split())
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sentences')
+    parser.add_argument('-filtered')
+    return parser.parse_args()
+
+
+def filter_sentence(sentence):
+    sentence = sentence.strip()
+    if not sentence:
+        return True
+    words = sentence.split()
+    if len(words) == 1:
+        return True
+
+    for ignore_start in IGNORE_START:
+        if sentence.startswith(ignore_start):
+            return True
+    return False
+
+
+def main():
+    args = setup_args()
+
+
+if __name__ == '__main__':
+    main()
